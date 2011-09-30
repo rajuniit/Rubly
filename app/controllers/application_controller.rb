aca464baef51c3724e9c6cfebb3045d0a3c1ea9c
Raju Mazumder <rajuniit@gmail.com>
2011-09-30 22:37:58 +0600

    changed template and added ajax functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def init_layout_data
+    @links = Link.all;
+  end
+  def page_title
+    "Rubly- Url Shortener"
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,12 +1,9 @@
 class LinksController < ApplicationController
 
+  before_filter :init_layout_data
   def index
-    @links = Link.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @links }
-    end
+   redirect_to '/links/new'
+   return
   end
 
 
@@ -35,16 +32,19 @@ class LinksController < ApplicationController
     @link.ip_address = request.remote_ip
     respond_to do |format|
       if @link.save
-        format.html { redirect_to(@link, :notice => 'Link was successfully created.') }
+        format.html { redirect_to('/', :notice => 'Link was successfully created.') }
         format.xml  { render :xml => @link, :status => :created, :location => @link }
+        format.js
       else
         format.html { render :action => "new" }
         format.xml  { render :xml => @link.errors, :status => :unprocessable_entity }
+        format.js
       end
     end
   end
 
   def render_link
+
     slug = params[:slug]
     @link = Link.new
     @link = @link.get_link_by_slug(slug)
